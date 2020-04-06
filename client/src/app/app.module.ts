40c8fd10cb042732332cc43d6b315f5a99d32433
ue85540 <heiko.henning@sbb.ch>
2020-04-06 09:40:30 +0200

    Basic angular + api

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BASE_PATH } from './api/variables';
+import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [{
+    provide: BASE_PATH,
+    useValue: environment.API_BASE_PATH
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
