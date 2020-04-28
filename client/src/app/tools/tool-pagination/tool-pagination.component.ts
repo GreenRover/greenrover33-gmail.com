1c95380bac84c0f74b48239e1d05dd96dee6ac65
ue85540 <heiko.henning@sbb.ch>
2020-04-28 21:45:56 +0200

    fur kai

---
+++ client/src/app/tools/tool-pagination/tool-pagination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-pagination',
+  templateUrl: './tool-pagination.component.html',
+  styleUrls: ['./tool-pagination.component.css']
+})
+export class ToolPaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/tools/tools.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolPaginationComponent } from './tool-pagination/tool-pagination.component';
+
+
+
+@NgModule({
+  declarations: [ToolPaginationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ToolsModule { }
