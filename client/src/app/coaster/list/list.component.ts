998f10a73ebbb2232e69786f95e220840dd77034
ue85540 <heiko.henning@sbb.ch>
2020-04-28 21:41:57 +0200

    schöne coaster liste

--- client/src/app/coaster/list/list.component.ts
+++ client/src/app/coaster/list/list.component.ts
@@ -1,3 +1,5 @@
+import { Page } from './../../api/model/page';
+import { Coaster } from './../../api/model/coaster';
 import { CoasterService } from './../../api/api/coaster.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  page: Page<Coaster> = {
+    content: [],
+  };
 
   constructor(
     private coasterApi: CoasterService
@@ -14,6 +19,7 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void {
     this.coasterApi.paged(1, 25).subscribe(page => {
+      this.page = page;
       console.log(page);
     });
   }
