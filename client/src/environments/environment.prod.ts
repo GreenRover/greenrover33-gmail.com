969a653341fcc7ddeb6b8cc86947bc3fb5bcb34c
Marcus <ms@mtrail.ch>
2020-06-10 13:22:46 +0000

    Docker-Experimente, Version bei Marcus

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
+//import 'angular-server-side-configuration/process';
+
 export const environment = {
-  production: true
+  production: true,
+  //API_BASE_PATH: process.env.API_BASE_PATH
+  API_BASE_PATH: "/api",
+  sbb_dmz_broker: {
+    url: 'wss://tms-mob-int01-public.sbb.ch:11443,wss://tms-mob-int02-public.sbb.ch:11443',
+    vpnName: 'default',
+    userName: 'default',
+    password: 'default',
+  }
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
